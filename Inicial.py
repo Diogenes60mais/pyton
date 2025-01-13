31129f783248839cc520eeacb6495c910c378b8d
diogenes60mais <diogenes60mais@gmail.com>
2025-01-12 23:02:21 -0300

    initial commit

---
+++ Inicial.py
@@ -0,0 +1,46 @@
+#impimir na tela
+print ('--------')
+print ('olá!')
+print ('---++-----')
+input ('Você gosta de estudar?')
+
+#variaveis
+"""
+idade = 64 # idade recebe 64
+print ('minha idade é de' (idade)sim)
+
+
+_____________________
+Cometarios de várias linhas
+_____________________
+"""
+#operações aritiméticas 
+ 
+num1 = 10
+num2 = 20
+
+print (num1 + num2)
+print (num1 - num2)
+
+idade = 18
+
+if idade >=18:
+    print ("você é maior de idade")
+else:
+    print ("Você é menor de idade.")
+
+
+media = float(input('Informe a media do estudantes:  '))
+presença = int(input('Informe a media do estudantes:  '))
+if media >= 7 and presença >=70:
+    print ('Você foi aprovado(a) parabens!')
+elif media >= 7 and presença < 70:
+    print ('Foi foi reprovada mais, não desista!')
+elif media > 5:
+    print ('Você está em recuperação, se esforce')
+elif media >= 7 and presença < 70:
+    print ('Foi foi reprovada mais, não desista!')
+else:
+    print ('Foi foi reprovada mais, não desista!')
+    
+
